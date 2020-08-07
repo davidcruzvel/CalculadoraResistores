dc5f308a32ad1f2dd59b9ce5340a83357efe4fa1
David Cruz Velasquez <david.cruzvelasquez@gmail.com>
2020-08-06 18:52:15 -0600

    - Se modificó el index.html   * Se les asignó valor a las etiquetas "value" de los select y los option   * Se agregaron comentarios para distinguir botones y resistores segun sus bandas   * Se incluyó los select option dentro de "form"
    - Se modificó el code.js
      * Se creó la funcion calcular3bandas();
      * Se realizaron pruebas de lectura e impresión de datos
    
    -> Funciona:
        - La lectura de variables
    
    -> No funciona:
        - La impresión de variables en la caja

--- js/code.js
+++ js/code.js
@@ -1,1 +1,17 @@
-/* AQUI SE ENCUENTRA EL CODIGO JS DEL PROYECTO */
+function calcular3bandas(){
+    var ban1 = document.f3bandas.b1.value;
+    var ban2 = document.f3bandas.b2.value;
+    var multipli = document.f3bandas.mult.value;
+    var valorcaja = 0;
+    var con = ban1.toString() + ban2.toString();
+    
+    valorcaja = parseFloat(con) * parseFloat(multipli);
+
+    document.f3bandas.caja.value=valorcaja;
+
+    console.log(ban1);
+    console.log(ban2);
+    console.log(multipli);
+    console.log(valorcaja);
+    console.log(con);
+}
