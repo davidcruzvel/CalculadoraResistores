3ff6bb38fef26ac09afd364a13dcbabf1fcc5174
David Cruz Velasquez <david.cruzvelasquez@gmail.com>
2020-08-06 20:58:36 -0600

    Resumen de cambios.
    -> Funciona:
        - Calculo de resistor de 3 y 4 bandas
    
    -> Pendientes:
        - Calculo del resistor de 5 bandas
        - Validacion de errores en los select
        - Identacion (Por ultimo)

--- js/code.js
+++ js/code.js
@@ -1,10 +1,4 @@
-function limpiar(){    
-    document.f3bandas.caja.value = "";
-    document.f3bandas.b1.value = -1;
-    document.f3bandas.b2.value = -1;
-    document.f3bandas.mult.value = -1;
-}
-
+//CALCULO DE RESISTOR DE 3 BANDAS
 
 function calcular3bandas(){
     var ban1 = document.f3bandas.b1.value;
@@ -14,9 +8,41 @@ function calcular3bandas(){
     var con = ban1.toString() + ban2.toString();
     
     valorcaja = parseFloat(con) * parseFloat(multipli);
-    console.log(valorcaja);
 
     valorcaja = valorcaja + " Ω";
 
     document.f3bandas.caja.value = valorcaja;
+}
+
+function limpiar3bandas(){    
+    document.f3bandas.caja.value = "";
+    document.f3bandas.b1.value = -1;
+    document.f3bandas.b2.value = -1;
+    document.f3bandas.mult.value = -1;
+}
+
+//CALCULO DE RESISTOR DE 4 BANDAS
+
+function calcular4bandas(){
+    var ban1 = document.f4bandas.b1.value;
+    var ban2 = document.f4bandas.b2.value;
+    var multipli = document.f4bandas.mult.value;
+    var tolerancia = document.f4bandas.tol.value;
+    var valorcaja = 0;
+    var con = ban1.toString() + ban2.toString();
+    
+    valorcaja = parseFloat(con) * parseFloat(multipli);
+
+    valorcaja = valorcaja + " Ω " + tolerancia;
+
+    document.f4bandas.caja.value = valorcaja;
+
+}
+
+function limpiar4bandas(){    
+    document.f4bandas.caja.value = "";
+    document.f4bandas.b1.value = -1;
+    document.f4bandas.b2.value = -1;
+    document.f4bandas.mult.value = -1;
+    document.f4bandas.tol.value = -1;
 }
